dfd313b2ad4318473a32a723558f006d4b845557
Kabulzhan <kabulzhan@mail.ru>
2020-11-25 07:43:58 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Main starting point of the application
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const app = express();
+const router = require("./router");
+const mongoose = require("mongoose");
+
+// DB Setup
+
+mongoose.connect(
+  `mongodb+srv://kabulzhan:${process.env.MONGODB_PASS}@cluster0.pnfqe.mongodb.net/test`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// App Setup
+app.use(morgan("combined"));
+app.use(bodyParser.json({ type: "*/*" }));
+router(app);
+
+// Server setup
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log("Server listening on: ", port);

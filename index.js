a8bc19f2c2659e81d18d8ccb733126ce936bda14
Kabulzhan <kabulzhan@mail.ru>
2020-11-25 15:29:56 +0600

    Add CORS library

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const morgan = require("morgan");
 const app = express();
 const router = require("./router");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB Setup
 
@@ -17,6 +18,7 @@ mongoose.connect(
 
 // App Setup
 app.use(morgan("combined"));
+app.use(cors());
 app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
